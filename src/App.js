ad210663c70ed3d8ca850ea2fea6a6e71c8e598b
marimuthue1302 <marimuthue1302@gmail.com>
2023-06-22 08:01:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -11,31 +11,45 @@ function App() {
   const [icon, setIcon] = useState("");
 
   const findWeather = async () => {
-    const apikey = "e87ebd97ab8692cfd5691edbbf9bf3e2";
-    const unit = "metric";
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      input +
-      "&appid=" +
-      apikey +
-      "&units=" +
-      unit;
-    const response = await axios.get(url);
-    const temp = await response.data.main.temp;
-    const description = await response.data.weather[0].description;
-    const icon = await response.data.weather[0].icon;
-    const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    setTemp(temp);
-    setDescription(description);
-    setIcon(imageURL);
-    setInput("");
+    try {
+      const apikey = "e87ebd97ab8692cfd5691edbbf9bf3e2";
+      const unit = "metric";
+      const url =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        input +
+        "&appid=" +
+        apikey +
+        "&units=" +
+        unit;
+      const response = await axios.get(url);
+      const temp = await response.data.main.temp;
+      const description = await response.data.weather[0].description;
+      const icon = await response.data.weather[0].icon;
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      setTemp(temp);
+      setDescription(description);
+      setIcon(imageURL);
+      setInput("");
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        alert("Enter the valid city name");
+      } else if (input === "") {
+        alert("Enter the city name");
+      }
+    }
   };
   return (
     <div className="app">
       {temp === "" ? (
         <Input setInput={setInput} findWeather={findWeather} />
       ) : (
-        <Result input={input} temp={temp} desc={description} icon={icon} setTemp={setTemp} />
+        <Result
+          input={input}
+          temp={temp}
+          desc={description}
+          icon={icon}
+          setTemp={setTemp}
+        />
       )}
     </div>
   );
